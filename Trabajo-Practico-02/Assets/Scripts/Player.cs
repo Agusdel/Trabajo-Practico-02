4619c7dc8b5ec3ab3df18b1fd4b2eeb413d1527a
PC-15\PC01 <agusdel_94@hotmail.com>
2016-09-20 12:09:34 -0300

    ! commit

---
+++ Trabajo-Practico-02/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Animator anim;
+    public bool haduken;
+
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+
+	}
+	
+	void Update ()
+    {
+        anim.SetBool("Haduken", haduken);
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            haduken = true;
+            Debug.Log("HAAADUUUKEEEEN!!!");
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            haduken = false;
+            Debug.Log("Idle.");
+        }
+
+    }
+}
---
+++ Trabajo-Practico-02/Assets/Scripts/PlayerPool.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerPool : MonoBehaviour {
+
+    public int poolSize = 10;
+    public GameObject player;
+    private List<GameObject> activeObjects;
+    private List<GameObject> poolObjects;
+
+    void Start ()
+    {
+        activeObjects = new List<GameObject>();
+        poolObjects = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(player);
+            poolObjects.Add(obj);
+            obj.SetActive(false);
+        }
+	}
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (poolObjects.Count > 0)
+            {
+                Spawn(poolObjects[poolObjects.Count - 1]);
+                Debug.Log("Pool object created.");
+            }
+            else
+            {
+                Debug.Log("Error creating pool object.");
+            }
+        }
+    }
+
+    public void Spawn(GameObject obj)
+    {
+        if (poolObjects.Count > 0)
+        {
+            obj.transform.position = new Vector2(Random.Range(-2f, 2f), Random.Range(-0.65f, -0.77f));
+            obj.SetActive(true);
+            activeObjects.Add(obj);
+            poolObjects.RemoveAt(poolObjects.Count - 1);
+        }
+    }
+
+    public void Destroy(GameObject pooledObject)
+    {
+        PlayerPool poolableObject = pooledObject.GetComponent<PlayerPool>();
+        if (pooledObject == null)
+        {
+            Debug.LogError("Trying to destroy a non poolable object");
+            return;
+        }
+        poolableObject.Destroy(pooledObject);
+    }
+}
